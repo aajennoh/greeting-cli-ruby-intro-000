cce2b42f2d2fdbe528ade0388cf47a9378095f89
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-05 17:45:01 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Hello #{name}.strip.chomp. It's nice to meet you."
-  gets.strip.chomp(name)
+  puts "Hello #{name}. It's nice to meet you."
+  name = "Sally\t"
 
 end
